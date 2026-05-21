14c61120de7506bf16d5a3300dea076da2ffcb34
justinkimm17 <183580285+DontUseCheats@users.noreply.github.com>
2026-05-20 20:11:51 -0700

    create undo function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+import json
+from pathlib import Path
 
 # create a categories Dict which contains all filetypes
 FILES_CATEGORIES = {
@@ -26,7 +28,7 @@ def get_category(file_type):
             return category
         return "Other"
 
-def get_size_buckets(size_bytes):
+def get_size_bucket(size_bytes):
     for category, low_range, high_range in SIZE_BUCKETS:
         if low_range <= size_bytes < high_range:
             return category
@@ -35,7 +37,32 @@ def get_size_buckets(size_bytes):
 # "folder" is folder path user wants to look at
 # "sort_by" is whether to sort by type, size or both
 def organize_file(folder, sort_by):
+    folder = Path(folder)
+    moves = []   # track every move
 
+    for file in folder.iterdir():
+        if file.is_file():
+            category = get_category(file.suffix)
+            size_bucket = get_size_bucket(file.stat().st_size)
+
+
+            # creates subfolder pathways in destination path
+            if sort_by == "type":
+                subfolder = folder / category
+            elif sort_by == "size":
+                subfolder = folder / size_bucket
+            else: # both
+                subfolder = folder / category / size_bucket
+
+            # creates actual folder on disk
+            subfolder.mkdir(parents=True, exist_ok=True)
+            destination = subfolder / file.name
+            shutil.move(str(file), str(destination)) # moves file to according destination
+            moves.append({"from": str(file), "to": str(destination)})
+
+            log_path = folder / ".organizer_log.json"
+            with open(log_path, "w") as f:
+                json.dump(moves, f)
 
 
 
